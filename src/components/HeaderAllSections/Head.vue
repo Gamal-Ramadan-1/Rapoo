22074a4a2028580ec689ca6daa89c8991c1a8a6a
Gamal-Ramadan-1 <gamal.ramadan.fe@gmail.com>
2022-10-14 21:10:36 +0200

    Create Head All of Sections

---
+++ src/components/HeaderAllSections/Head.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="row">
+    <div class="col-12 text-center">
+      <h1 class="text-capitalize my-2">{{ MainHead }}</h1>
+      <p class="text-capitalize my-2">
+        {{ Descripsion }}
+        <span class="d-lg-block d-inline">{{ Remainder }}</span>
+      </p>
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  name: "Head",
+  props: {
+    MainHead: String,
+    Descripsion: String,
+    Remainder: String,
+  },
+};
+</script>
+<style lang="scss" scoped>
+@import "@/SCSS/Main.scss";
+p {
+  color: #6c757d;
+}
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -5,9 +5,10 @@
 <script>
 import SliderHome from "@/components/Sliders/SliderHome.vue";
 import Service from "../components/Service/Service.vue";
+import Head from "@/components/HeaderAllSections/Head.vue";
 export default {
   name: "Home-1",
-  components: { SliderHome, Service },
+  components: { SliderHome, Service, Head },
 };
 </script>
 <style lang="scss" scoped>
