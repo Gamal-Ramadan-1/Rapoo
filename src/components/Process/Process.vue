1527fa7dfbd56f39b9ec33be50b24471cd62fc6e
Gamal-Ramadan-1 <gamal.ramadan.fe@gmail.com>
2022-10-16 19:51:30 +0200

    Finish Process Section

---
+++ src/components/Process/Process.vue
@@ -0,0 +1,57 @@
+<template>
+  <section class="process">
+    <div class="container">
+      <Head
+        MainHead="Our Working Process"
+        Descripsion="Rapoo can be used to create anything from a small marketing page to a"
+        Remainder=" sophisticated website."
+      />
+      <div class="row py-5 gy-4 justify-content-lg-start justify-content-center">
+        <div class="col-lg-4 col-md-6 col-sm-12 text-center">
+          <img src="@/Imgs/process-1.jpg" class="img-fluid" />
+          <h4 class="text-capitalize mt-4">project research</h4>
+          <p>
+            Nihil facere delectus eaque aut possimus nobis laudantium
+            reprehenderit
+          </p>
+        </div>
+        <div class="col-lg-4 col-md-6 col-sm-12 text-center">
+          <img src="@/Imgs/process-2.jpg" class="img-fluid" />
+          <h4 class="text-capitalize mt-4">project demostration</h4>
+          <p>
+            Nihil facere delectus eaque aut possimus nobis laudantium
+            reprehenderit
+          </p>
+        </div>
+        <div class="col-lg-4 col-md-6 col-sm-12 text-center">
+          <img src="@/Imgs/process-3.jpg" class="img-fluid" />
+          <h4 class="text-capitalize mt-4">Development & delivery</h4>
+          <p>
+            Nihil facere delectus eaque aut possimus nobis laudantium
+            reprehenderit
+          </p>
+        </div>
+      </div>
+    </div>
+  </section>
+</template>
+<script>
+import Head from "../HeaderAllSections/Head.vue";
+export default {
+  name: "Process",
+  components: { Head },
+};
+</script>
+<style lang="scss" scoped>
+@import "@/SCSS/Main.scss";
+.process {
+  padding: 100px 0px;
+  img {
+    object-fit: cover;
+    border-radius: 10px;
+  }
+  p {
+    color: #6c757d;
+  }
+}
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,14 +1,16 @@
 <template>
   <SliderHome />
   <Service />
+  <Process />
 </template>
 <script>
 import SliderHome from "@/components/Sliders/SliderHome.vue";
 import Service from "../components/Service/Service.vue";
 import Head from "@/components/HeaderAllSections/Head.vue";
+import Process from "@/components/Process/Process.vue";
 export default {
   name: "Home-1",
-  components: { SliderHome, Service, Head },
+  components: { SliderHome, Service, Head, Process },
 };
 </script>
 <style lang="scss" scoped>
