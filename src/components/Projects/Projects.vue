728cf51ad98b259bbfa4a95dacb9b0d221d6df6b
Gamal-Ramadan-1 <gamal.ramadan.fe@gmail.com>
2022-10-19 20:31:26 +0200

    Finished Projects Section

---
+++ src/components/Projects/Projects.vue
@@ -0,0 +1,153 @@
+<template>
+  <section class="projects-top">
+    <div class="container">
+      <div class="row">
+        <div class="col-lg-6 col-sm-12">
+          <h1 class="text-white">
+            We are trying to deliver 100% quality product and success their
+            business to achieve ultimate goal.
+          </h1>
+        </div>
+        <div class="col-lg-6 col-sm-12">
+          <p>
+            We are providing best service since 1990 to present , committed to
+            best service delivery.Nullam metus enim, placerat in lacus vel,
+            porttitor egestas libero. Etiam ex risus, feugiat eget accumsan eu,
+            sagittis eu urna. In eget ultrices metus. Nunc accumsan
+          </p>
+        </div>
+      </div>
+    </div>
+  </section>
+  <section class="projects-bottom">
+    <div class="container">
+      <div class="row gy-4 justify-content-lg-first justify-content-center">
+        <div class="col-lg-4 col-md-6 text-center">
+          <img src="@/Imgs/p-1.jpg" class="img-fluid" />
+          <h5 class="my-4 text-capitalize">project: onepage</h5>
+          <p class="text-center">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatum,
+            excepturi debitis consectetur blanditiis fuga vitae placeat
+            quibusdam
+          </p>
+        </div>
+        <div class="col-lg-4 col-md-6 text-center">
+          <img src="@/Imgs/p-4.jpg" class="img-fluid" />
+          <h5 class="my-4 text-capitalize">project: business</h5>
+          <p class="text-center">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatum,
+            excepturi debitis consectetur blanditiis fuga vitae placeat
+            quibusdam
+          </p>
+        </div>
+        <div class="col-lg-4 col-md-6 text-center">
+          <img src="@/Imgs/p-3.jpg" class="img-fluid" />
+          <h5 class="my-4 text-capitalize">project: marketing</h5>
+          <p class="text-center">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatum,
+            excepturi debitis consectetur blanditiis fuga vitae placeat
+            quibusdam
+          </p>
+        </div>
+      </div>
+      <div class="row mt-5 pt-3 gy-5">
+        <div class="col-lg-7 col-md-12 col-sm-12">
+          <div class="single-project">
+            <img src="@/Imgs/banner_bg.jpg" class="img-fluid" />
+          </div>
+        </div>
+        <div class="col-lg-5 position-relative col-md-12 col-sm-12">
+          <div class="about-us">
+            <div class="content">
+              <h5>do you know who we are ?</h5>
+              <h2 class="my-4">More than just service your products...</h2>
+              <p>
+                Explain to you how all this mistaken idea of denouncing pleasure
+                and praising pain was born and we will give you a complete
+                account of the system.
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+</template>
+<script>
+export default {
+  name: "Projects",
+};
+</script>
+<style lang="scss" scoped>
+@import "@/SCSS/Main.scss";
+.projects-top {
+  background-color: $Dark;
+  padding: 80px 0px 180px 0px;
+  margin-bottom: 30px;
+  p {
+    font-size: 18px;
+    color: rgb(177, 176, 176);
+  }
+}
+.projects-bottom {
+  margin-top: -130px;
+  padding-bottom: 130px;
+  img {
+    border-radius: 10px;
+    object-fit: cover;
+    margin-bottom: 20px;
+  }
+  p {
+    color: rgb(122, 122, 122);
+  }
+  .single-project {
+    img {
+      object-fit: cover;
+      height: 450px;
+      width: 100%;
+    }
+  }
+  .about-us {
+    .content {
+      background-color: $Snowy;
+      padding: 70px 25px;
+      h5 {
+        color: $Green;
+        font-size: 18px;
+        font-weight: bold;
+        text-transform: capitalize;
+      }
+    }
+  }
+}
+@media only screen and (max-width: 4000px) {
+  .about-us {
+    padding: 16px;
+    width: 500px;
+    background-color: white;
+    position: absolute;
+    top: 120px;
+    left: -200px;
+    box-shadow: 0px 0px 10px 5px #eee;
+  }
+}
+@media only screen and (max-width: 1024px) {
+  .about-us {
+    top: 100px;
+    left: -110px;
+  }
+}
+@media only screen and (max-width: 768px) {
+  .about-us {
+    width: auto;
+    position: static;
+    top: 0px;
+    left: 0px;
+  }
+}
+@media only screen and (max-width: 426px) {
+  .content {
+    text-align: center;
+  }
+}
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -4,6 +4,7 @@
   <Process />
   <Services2 />
   <Pricing />
+  <Projects />
 </template>
 <script>
 import SliderHome from "@/components/Sliders/SliderHome.vue";
@@ -12,9 +13,18 @@ import Head from "@/components/HeaderAllSections/Head.vue";
 import Process from "@/components/Process/Process.vue";
 import Services2 from "@/components/WebServices/Services2.vue";
 import Pricing from "@/components/Pricing/Pricing.vue";
+import Projects from "@/components/Projects/Projects.vue";
 export default {
   name: "Home-1",
-  components: { SliderHome, Service, Head, Process, Services2, Pricing },
+  components: {
+    SliderHome,
+    Service,
+    Head,
+    Process,
+    Services2,
+    Pricing,
+    Projects,
+  },
 };
 </script>
 <style lang="scss" scoped>
